cc3396b5f7ff1da00099a2fa3bd57c8b9a0fba39
redihoxhaa <redi.hoxha1996@gmail.com>
2024-01-08 15:37:39 +0100

    feat: esercizio completato

--- index.php
+++ index.php
@@ -7,17 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Import Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- /Import Bootstrap -->
-
-    <!-- Import Fonts -->
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- /Font Awesome -->
-    <!-- /Import Fonts -->
 
     <!-- Import CSS -->
     <link rel="stylesheet" href="css/style.css">
@@ -30,29 +21,38 @@
 </head>
 
 <body>
+    <div class="container mt-5">
+        <!-- Header -->
+        <header>
+            <h1 class="text-center text-uppercase">Don't say it!</h1>
+        </header>
+        <!-- /Header -->
 
-    <!-- Header -->
-    <header>
-<h1>Don't say it!</h1>
-    </header>
-    <!-- /Header -->
+        <!-- Main -->
+        <main>
+            <form action="result.php" method="post" class="d-flex flex-column">
+                <div class="mb-3">
+                    <label for="badword" class="form-label text-uppercase">Bad Word</label>
+                    <input type="text" class="form-control" id="badword" name="badword" placeholder="Write the bad word here...">
+                </div>
+                <div class="mb-3">
+                    <label for="paragraph" class="form-label text-uppercase">Paragraph</label>
+                    <textarea class="form-control" id="paragraph" name="paragraph" rows="3" placeholder="Write your paragraph here..."></textarea>
+                </div>
+                <button class="btn btn-primary mt-2">Submit</button>
+            </form>
 
-    <!-- Main -->
-    <main>
+        </main>
+        <!-- /Main -->
 
-    </main>
-    <!-- /Main -->
+        <!-- Footer -->
+        <footer>
 
-    <!-- Footer -->
-    <footer>
-
-    </footer>
-    <!-- /Footer -->
-
+        </footer>
+        <!-- /Footer -->
+    </div>
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!-- /Bootstrap JS -->
 
 </body>
--- result.php
+++ result.php
@@ -1,3 +1,9 @@
+<?php
+$badword = $_POST["badword"];
+$paragraph = $_POST["paragraph"];
+$modified_paragraph = str_replace($badword, '***', $paragraph);
+$counter = substr_count($paragraph, $badword);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,17 +13,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Import Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- /Import Bootstrap -->
-
-    <!-- Import Fonts -->
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- /Font Awesome -->
-    <!-- /Import Fonts -->
 
     <!-- Import CSS -->
     <link rel="stylesheet" href="css/style.css">
@@ -30,29 +27,69 @@
 </head>
 
 <body>
+    <div class="container mt-5">
+        <!-- Header -->
+        <header>
+            <h1 class="text-center text-uppercase">Don't say it!</h1>
+        </header>
+        <!-- /Header -->
 
-    <!-- Header -->
-    <header>
-<h1>Don't say it!</h1>
-    </header>
-    <!-- /Header -->
+        <!-- Main -->
+        <main>
+            <div class="text-uppercase mb-5"> Your paragraph is:
+                <div class="result">
+                    <?php echo $paragraph; ?>
+                </div>
+            </div>
 
-    <!-- Main -->
-    <main>
+            <div class="text-uppercase mb-5"> The paragraph is
+                <div class="result">
+                    <?php echo strlen($paragraph); ?>
+                </div>
+                characters long
+            </div>
 
-    </main>
-    <!-- /Main -->
+            <hr>
 
-    <!-- Footer -->
-    <footer>
+            <div class="text-uppercase mt-5 mb-5"> Your 🤭 badword is:
+                <div class="result badword">
+                    <?php echo $badword; ?>
+                </div>
+                and it occurs
+                <div class="result">
+                    <?php echo $counter; ?>
+                </div>
+                times
+            </div>
 
-    </footer>
-    <!-- /Footer -->
+            <div class="text-uppercase mb-5"> Your modified paragraph is:
+                <div class="result">
+                    <?php echo $modified_paragraph; ?>
+                </div>
+            </div>
 
+            <div class="text-uppercase mb-5"> The modified paragraph is
+                <div class="result">
+                    <?php echo strlen($modified_paragraph); ?>
+                </div>
+                characters long counting "***", and
+                <div class="result">
+                    <?php echo strlen($modified_paragraph) - (3 * $counter); ?>
+                </div>
+                without counting them
+            </div>
+
+        </main>
+        <!-- /Main -->
+
+        <!-- Footer -->
+        <footer>
+
+        </footer>
+        <!-- /Footer -->
+    </div>
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!-- /Bootstrap JS -->
 
 </body>
